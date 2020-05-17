e7e9afb9ed6409774eb9d6942645c5fce37c2083
Ray Park <pkoarmy@verizonmedia.com>
2020-05-17 16:25:28 -0700

    season tells

---
+++ week2/week3/season.py
@@ -0,0 +1,27 @@
+# https://github.com/pkoarmy/Learning-Python/blob/master/week1/indent1.py
+
+import random
+
+print("We are going to calculate season from given Month")
+print("We call Sping if the month between March and May")
+print("We call Jun - Aug as Summer")
+print("We call Sep - Nov as Fall")
+print("We call Dec - Feb as Winter")
+
+month = random.randint(1, 12)
+
+print("current month is ", month)
+print("So it is")
+
+# You remember this code what we learned on our class
+'''
+if month == 1:
+   print("January")
+elif month == 2:
+   print("February")
+elif month == 3:
+   print("March")
+'''
+
+# pleae complete the Python code to print out what is the season from month
+# hint : you can use logical operator with if clause
